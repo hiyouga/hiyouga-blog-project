5faf0274353ed9bf2782c29235e9eb873aad29e4
hiyouga <467089858@qq.com>
2018-01-30 22:18:05 +0800

    1801302218

--- libs/js/core.js
+++ libs/js/core.js
@@ -1,7 +1,32 @@
+var GetUrlValue = function(name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        try {
+            return decodeURIComponent(r[2]);
+        } catch (e) {
+            return null;
+        }
+    }
+    return null;
+}
+
 $(document).ready(function(){
-	GetIssues();
-	GetArticles();
+	if(issueid = GetUrlValue('issues')){
+		GetOneIssue(issueid);
+	}else{
+		GetIssues();
+		GetArticles();
+	}
 });
+
+function GetOneIssue(i){
+	$.get("https://api.github.com/repos/hiyouga/hiyouga-blog-project/issues/"+i, function(data){
+		$("#data #title").text(data.title);
+		$("#data #content").text(data.body.replace('\\r\\n','<br />'));
+	});
+}
+
 function GetIssues(){
 	$.get("https://api.github.com/repos/hiyouga/hiyouga-blog-project/issues", function(data){
 		$("#issues").html("");
@@ -10,6 +35,7 @@ function GetIssues(){
 		});
 	});
 }
+
 function GetArticles(){
 	$.get("https://api.github.com/repositories/91178023/contents/articles", function(data){
 		$("#articles").html("");
