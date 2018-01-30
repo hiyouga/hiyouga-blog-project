cf9b1f4e57e5d01d8829d5131b9a3801038c9b29
hiyouga <467089858@qq.com>
2018-01-30 22:26:52 +0800

    1801302224

--- libs/js/core.js
+++ libs/js/core.js
@@ -23,24 +23,23 @@ $(document).ready(function(){
 function GetOneIssue(i){
 	$.get("https://api.github.com/repos/hiyouga/hiyouga-blog-project/issues/"+i, function(data){
 		$("#data #title").text(data.title);
-		$("#data #content").text(data.body.replace('\\r\\n','<br />'));
+		$("#data #content").text(data.body.replace('\r\n','<br />'));
 	});
 }
 
 function GetIssues(){
 	$.get("https://api.github.com/repos/hiyouga/hiyouga-blog-project/issues", function(data){
-		$("#issues").html("");
 		$.each(data,function(index,item){
-			$("#issues").append("<li><a href=\"/?issues=" + item.number + "\">" + item.title + "</a></li>");
+			$("#list #issues").append("<li><a href=\"/?issues=" + item.number + "\">" + item.title + "</a></li>");
 		});
 	});
 }
 
 function GetArticles(){
 	$.get("https://api.github.com/repositories/91178023/contents/articles", function(data){
-		$("#articles").html("");
+		$("#list #articles").html("");
 		$.each(data, function(index, item){
-			$("#articles").append("<li><a target=\"_blank\" href=\"" + item.path + "\">" + item.name + "</a></li>");
+			$("#list #articles").append("<li><a target=\"_blank\" href=\"" + item.path + "\">" + item.name + "</a></li>");
 		});
 	});
 }
