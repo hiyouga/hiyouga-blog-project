d5a94325909892d30f26d98701887d62282521d4
hiyouga <467089858@qq.com>
2018-01-31 23:43:46 +0800

    V0.0.2
    返回键
    时间修复

--- libs/js/core.js
+++ libs/js/core.js
@@ -1,20 +1,15 @@
-var GetUrlValue = function(name) {
-    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
-    var r = window.location.search.substr(1).match(reg);
-    if (r != null) {
-        try {
-            return decodeURIComponent(r[2]);
-        } catch (e) {
-            return null;
-        }
-    }
-    return null;
-}
+// AJAX cache on
+$(function(){
+     $.ajaxSetup ({
+         cache: true
+     });
+});
 
+// Main
 $(document).ready(function(){
 	switch(GetUrlValue('type')){
 		case '1':
-			GetOneIssue(GetUrlValue('aid'));
+			GetOneBlog(GetUrlValue('aid'));
 			break;
 		case '2':
 			break;
@@ -27,26 +22,27 @@ $(document).ready(function(){
 	}
 	GetBlogList();
 });
+// Methods
+function GetNone(){
+	;
+}
 
-function GetOneIssue(i){
+function GetOneBlog(i){
 	$("#mainloader").show();
 	$.get("https://api.github.com/repos/hiyouga/hiyouga-blog-project/issues/"+i, function(data){
 		$("#mainloader").hide();
+		$("#backbtn").show();
 		$("#blog-main").addClass("blog-main-after");
 		//reg = new RegExp("\\r\\n", "g");
 		html = "<div class=\"p-2 blog-content\">";
 		html += "<h3 class=\"blog-post-title\">" + data.title + "</h3>";
-		html += "<p class=\"blog-post-meta\">" + data.created_at + " by <a href=\"" + data.user.html_url + "\">" + data.user.login + "</a></p>";
+		html += "<p class=\"blog-post-meta\">" + ConvTime(data.created_at) + " by <a href=\"" + data.user.html_url + "\">" + data.user.login + "</a></p>";
 		//blogtext = data.body.replace(reg, '<br />');
 		blogtext = data.body;
 		html += "<p>" + marked(blogtext) + "</p>";
 		html += "</div><!-- /.blog-post -->";
 		$("#blog-main").append(html);
 	});
-}
-
-function GetNone(){
-	;
 }
 
 function GetBlogs(){
@@ -56,7 +52,7 @@ function GetBlogs(){
 		$.each(data, function(index, item){
 			html = "<div class=\"p-2 blog-post\">";
 			html += "<h3 class=\"blog-post-title\">" + item.title + "</h3>";
-			html += "<p class=\"blog-post-meta\">" + item.created_at + " by <a href=\"" + item.user.html_url + "\">" + item.user.login + "</a></p>";
+			html += "<p class=\"blog-post-meta\">" + ConvTime(item.created_at) + " by <a href=\"" + item.user.html_url + "\">" + item.user.login + "</a></p>";
 			var Length = item.body.length;
 			var maxn = 200;
 			if(Length > maxn){
@@ -73,7 +69,7 @@ function GetBlogs(){
 
 function GetBlogList(){
 	$("#sideloader").show();
-	$.get("https://api.github.com/repos/hiyouga/hiyouga-blog-project/issues", function(data){
+	$.get("https://api.github.com/repos/hiyouga/hiyouga-blog-project/issues?state=open", function(data){
 		$("#sideloader").hide();
 		$.each(data,function(index,item){
 			$("#side-list").append("<li><a href=\"?type=1&aid=" + item.number + "\">" + item.title + "</a></li>");
@@ -90,23 +86,41 @@ function GetArticles(){
 		});
 	});
 }
+// Functions
+var GetUrlValue = function(name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        try {
+            return decodeURIComponent(r[2]);
+        } catch (e) {
+            return null;
+        }
+    }
+    return null;
+}
 
-$("body").on("click", "img", function(){
-	if($(this).attr("src") == undefined){
-		//alert($(this).attr("src"));
-		var id = $(this).attr("id");
-		var img = $(this).attr("imgdata");
-		var url = "img.php?url=" + img;
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET",url,true);
-		xhr.responseType = "blob";
-		xhr.onload = function() {
-			if (this.status == 200) {
-				var blob = this.response;
-				src = window.URL.createObjectURL(blob);
-				$("#"+id).attr("src",src);
-			}
-		}
-		xhr.send();
-	}
-});
+Date.prototype.Format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1,
+        "d+": this.getDate(),
+        "H+": this.getHours(),
+        "m+": this.getMinutes(),
+        "s+": this.getSeconds(),
+        "q+": Math.floor((this.getMonth() + 3) / 3),
+        "S": this.getMilliseconds()
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
+function ConvTime(str){
+	nst = new Date(str);
+	return nst.Format("yyyy-MM-dd HH:mm:ss");
+}
+
+function goback(){
+	window.history.go(-1);
+}
