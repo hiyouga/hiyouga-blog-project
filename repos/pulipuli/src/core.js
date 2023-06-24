647b2b20b05d64190496d2bc9c15f91c9be6d1d0
hiyouga <hiyouga@buaa.edu.cn>
2023-06-24 11:48:32 +0800

    support video parts

--- repos/pulipuli/src/core.js
+++ repos/pulipuli/src/core.js
@@ -1,78 +1,90 @@
 "use strict";
 
 var GetUrlValue = function (name) {
-	var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
-	var r = window.location.search.substring(1).match(reg);
-	if (r != null) {
-		try {
-			return decodeURIComponent(r[2]);
-		} catch (e) {
-			return null;
-		}
-	}
-	return null;
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = window.location.search.substring(1).match(reg);
+    if (r != null) {
+        try {
+            return decodeURIComponent(r[2]);
+        } catch (e) {
+            return null;
+        }
+    }
+    return null;
 }
 
 function matchlink(text, reg, urlbase) {
-	var allmatch = text.match(reg);
-	if (allmatch) {
-		for (var j = 0; j < allmatch.length; j++) {
-			text = text.replace(allmatch[j], "<a class=\"reproduce\" target=\"_blank\" href=\"" + urlbase + allmatch[j] + "\">" + allmatch[j] + "</a>")
-		}
-	}
-	return text
+    var allmatch = text.match(reg);
+    if (allmatch) {
+        for (var j = 0; j < allmatch.length; j++) {
+            text = text.replace(allmatch[j], "<a class=\"reproduce\" target=\"_blank\" href=\"" + urlbase + allmatch[j] + "\">" + allmatch[j] + "</a>")
+        }
+    }
+    return text
 }
 
 function autolink(text) {
-	text = matchlink(text, new RegExp('av\\d+', 'ig'), "https://www.bilibili.com/video/");
-	text = matchlink(text, new RegExp('sm\\d+', 'ig'), "https://www.nicovideo.jp/watch/");
-	text = matchlink(text, new RegExp('v=\\S+', 'ig'), "https://www.youtube.com/watch?");
-	return text
+    text = matchlink(text, new RegExp('av\\d+', 'ig'), "https://www.bilibili.com/video/");
+    text = matchlink(text, new RegExp('sm\\d+', 'ig'), "https://www.nicovideo.jp/watch/");
+    text = matchlink(text, new RegExp('v=\\S+', 'ig'), "https://www.youtube.com/watch?");
+    return text
 }
 
 function raise() {
-	$('#tips').modal("show");
-	setTimeout(function () {
-		window.location.reload();
-	}, 3000);
+    $('#tips').modal("show");
+    setTimeout(function () {
+        window.location.reload();
+    }, 3000);
 }
 
 $(document).ready(function () {
-	var vcode = GetUrlValue("vcode");
-	$.getJSON("src/video.json", function (jsondata) {
-		if (jsondata[vcode] == undefined) {
-			for (var key in jsondata) {
-				if (jsondata[key]["is_public"] == true) {
-					$("#vlist").append("<div class=\"col mb-4\"><div class=\"card h-100\"><a href=\"?vcode="
-						+ key + "\"><img src=\""
-						+ jsondata[key]["poster"] + "\" class=\"card-img-top\" /><div class=\"card-body\"><h5 class=\"card-title\">"
-						+ jsondata[key]["title"] + "</h5><p class=\"card-text\">"
-						+ jsondata[key]["desc"] + "</p></div></a></div></div>");
-				}
-			}
-		} else {
-			$.ajax({
-				type: "POST",
-				dataType: "json",
-				url: "https://bhpan.buaa.edu.cn/api/v1/link?method=osdownload",
-				data: JSON.stringify(jsondata[vcode]["postdata"]),
-				success: function(data, status) {
-					var link = data.authrequest[1];
-					$("#player").show();
-					$("#vinfo").show();
-					$("#dl-btn").show();
-					$("#dl-btn").attr("href", link);
-					$("#vcontent").attr("src", link);
-					$("#player").attr("data-poster", jsondata[vcode]["poster"]);
-					$("#vtitle").text(jsondata[vcode]["title"]);
-					$("#vdesc").html(autolink(jsondata[vcode]["desc"]));
-					var player = new Plyr("#player");
-				},
-				error: function(xhr) {
-					console.log(xhr.status + xhr.statusText);
-					raise();
-				}
-			});
-		}
-	});
+    var vcode = GetUrlValue("vcode");
+    var pid = Number(GetUrlValue("pid"));
+    $.getJSON("src/video.json", function (jsondata) {
+        if (jsondata[vcode] == undefined) {
+            for (var key in jsondata) {
+                if (jsondata[key]["is_public"] == true) {
+                    buttons = "";
+                    for (var i = 0; i < jsondata[key]["videos"].length; i++) {
+                        buttons += "<a class=\"btn btn-primary\" href=\"?vcode=" + key + "&pid=" + i + "\">" + i + "</a>";
+                    }
+                    $("#vlist").append(
+                        "<div class=\"col mb-4\">" +
+                            "<div class=\"card h-100\">" +
+                                "<img src=\"" + jsondata[key]["poster"] + "\" class=\"card-img-top\" />" +
+                                "<div class=\"card-body\">" +
+                                    "<h5 class=\"card-title\">" + jsondata[key]["title"] + "</h5>" +
+                                    "<p class=\"card-text\">" + jsondata[key]["desc"] + "</p>" +
+                                    buttons +
+                                "</div>" +
+                            "</div>" +
+                        "</div>"
+                    );
+                }
+            }
+        } else {
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: "https://bhpan.buaa.edu.cn/api/v1/link?method=osdownload",
+                data: JSON.stringify(jsondata[vcode]["videos"][pid]),
+                success: function (data) {
+                    var link = data.authrequest[1];
+                    $("#player").show();
+                    $("#vinfo").show();
+                    $("#dl-btn").show();
+                    $("#dl-btn").attr("href", link);
+                    $("#vcontent").attr("src", link);
+                    $("#player").attr("data-poster", jsondata[vcode]["poster"]);
+                    $("#vtitle").text(jsondata[vcode]["title"]);
+                    $("#vdesc").html(autolink(jsondata[vcode]["desc"]));
+                    new Plyr("#player");
+                },
+                error: function (xhr) {
+                    console.log(xhr.status + xhr.statusText);
+                    raise();
+                }
+            });
+        }
+    });
 });
